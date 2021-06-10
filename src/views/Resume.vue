b3f616c7ab0fffee6ee2c4cf499e4b58746a28e9
Jackson Lauder <jacksonelauder@gmail.com>
2021-06-09 21:05:07 -0600

    safd

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,6 +1,24 @@
 <template>
   <v-container fluid>
-    <h1>This is the Resume page</h1>
-    <iframe src="../assets/Resume Jun 2020.pdf" style="display:block; width:100%; height:100vh;" allowfullscreen></iframe>
+    <v-card>
+      <v-card-title><h2>My Resume</h2></v-card-title>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-img :src="require('../assets/Resume Jun 2020.png')"></v-img>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn dark depressed large color="blue-grey darken-2">
+          <v-icon class="mr-1">mdi-download</v-icon>
+          Download
+        </v-btn>
+
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
