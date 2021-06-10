1846bb2e47c9e62d65fb9d9232be449a0e4c3fe0
Jackson Lauder <jacksonelauder@gmail.com>
2021-06-10 14:54:42 -0600

    fsgd

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:jacksonlauder/jaxium-personal-website.git master:gh-pages
+git push -f git@github.com:jacksonlauder/jacksonlauder.github.io.git master:gh-pages
 cd -
