8bb4e1ba8a44cd99d50ef76a070ba39f8ebff167
Jackson Lauder <jacksonelauder@gmail.com>
2021-06-10 15:27:30 -0600

    Resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,14 +1,20 @@
 <template>
   <v-container fluid>
-    <v-card class="pa-6">
+    <v-card class="pa-8 mx-auto" max-width="65rem">
       <v-img :src="require('../assets/Resume Jun 2020.png')"></v-img>
 
-      <v-divider class="mx-4 my-7"></v-divider>
+      <v-divider class="mx-1 my-7"></v-divider>
 
       <v-card-actions>
         <v-spacer></v-spacer>
 
-        <v-btn dark depressed large color="blue-grey darken-2">
+        <v-btn
+          dark
+          depressed
+          large
+          color="blue-grey darken-2"
+          href="/Jackson_Lauder_Resume.pdf"
+        >
           <v-icon class="mr-1">mdi-download</v-icon>
           Download
         </v-btn>
