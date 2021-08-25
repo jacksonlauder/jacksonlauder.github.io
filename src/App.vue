9e4ade2e00ec25d239a73cf3ce03524c89fe04d0
Jackson Lauder <jacksonelauder@gmail.com>
2021-08-24 18:41:21 -0600

    open conditionally

--- src/App.vue
+++ src/App.vue
@@ -7,33 +7,31 @@
         <v-icon class="icon-logo"></v-icon>
       </v-btn>
 
-      <v-btn text plain class="app-bar-links font-weight-regular" :ripple="false" to="/resume">
+      <v-btn
+        text
+        plain
+        class="app-bar-links font-weight-regular"
+        :ripple="false"
+        to="/resume"
+      >
         <span>Resume</span>
       </v-btn>
 
-      <v-btn text plain class="app-bar-links font-weight-regular" :ripple="false" to="/projects">
+      <v-btn
+        text
+        plain
+        class="app-bar-links font-weight-regular"
+        :ripple="false"
+        to="/projects"
+      >
         <span>Projects</span>
       </v-btn>
 
-      <v-btn
-        icon
-        x-large
-        plain
-        :ripple="false"
-        href="https://github.com/jacksonlauder"
-        target="_blank"
-      >
+      <v-btn icon x-large plain :ripple="false" @click="goToGithub()">
         <v-icon>mdi-github</v-icon>
       </v-btn>
 
-      <v-btn
-        icon
-        x-large
-        plain
-        :ripple="false"
-        href="https://www.linkedin.com/in/jacksonlauder/"
-        target="_blank"
-      >
+      <v-btn icon x-large plain :ripple="false" @click="goToLinkedIn()">
         <v-icon>mdi-linkedin</v-icon>
       </v-btn>
 
@@ -51,7 +49,33 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    gitHubWindow: null,
+    linkedInWindow: null,
   }),
+
+  methods: {
+    goToGithub: function () {
+      // If a window with the name already exists, then url is loaded into the existing window.
+      if (this.gitHubWindow === null || this.gitHubWindow.closed) {
+        this.gitHubWindow = window.open(
+          "https://github.com/jacksonlauder",
+          "gitHubWindow"
+        );
+      } else {
+        this.gitHubWindow.focus();
+      }
+    },
+    goToLinkedIn: function () {
+      // If a window with the name already exists, then url is loaded into the existing window.
+      if (this.linkedInWindow === null || this.linkedInWindow.closed) {
+        this.linkedInWindow = window.open(
+          "https://www.linkedin.com/in/jacksonlauder/",
+          "linkedInWindow"
+        );
+      } else {
+        this.linkedInWindow.focus();
+      }
+    },
+  },
 };
 </script>
